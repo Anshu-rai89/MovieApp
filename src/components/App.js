7b5807d7ced4801d4ca1631c87424703584a835d
Anshu <raianshu1998@gmail.com>
2020-04-01 14:54:01 -0400

    added moviecardComponent

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from './navbar';
+import {data} from '../data';
+import MovieCard  from './movieCard';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <div className='main'>
+        
+        <div className='tabs'>
+          <div className='tab'>Movies</div>
+          <div className='tab'>Favourite</div>
+        </div>
+
+        <div  className='list'>
+           
+           {data.map((movie)=>
+               <MovieCard  movie={movie}/>
+           )}
+        </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+
+class Navbar extends React.Component
+{
+
+
+    render()
+    {
+        return (
+            <div className="nav">
+               <div className='search-container'>
+
+                   <input />
+                   <button id='search-btn' >search </button>
+               </div>
+            </div>
+          );
+    }
+  
+}
+
+export default Navbar;
